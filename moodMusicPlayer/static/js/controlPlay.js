20cf5e55a48dd3cc2a1fb528e7fa3435bb278f12
fluid-coder <130135533+fluid111@users.noreply.github.com>
2025-10-09 14:43:56 +0545

    progress aligned with song control

--- moodMusicPlayer/static/js/controlPlay.js
+++ moodMusicPlayer/static/js/controlPlay.js
@@ -10,7 +10,9 @@ let control =  document.getElementById("controlMusic")
 
 song.onloadedmetadata = function (){
     progress.max = song.duration;
+    console.log('max:', progress.max, 'seconds');
     progress.value = song.currentTime;
+    console.log('min:', progress.value, 'seconds');
     console.log('Duration:', song.duration, 'seconds');
 }
 
@@ -18,15 +20,27 @@ document.getElementById("controlMusic").addEventListener('click', playPause);
 function playPause(){
     if(control.classList.contains("fa-play")){
         song.play();
-        // control.classList.remove("fa-play");
-        // control.classList.add("fa-pause");
         control.classList.replace("fa-play", "fa-pause");
     }
     else{
         song.pause();
-        // control.classList.remove("fa-pause");
-        // control.classList.add("fa-play");
         control.classList.replace("fa-pause", "fa-play");
     }
+}
+
+// song.addEventListener("timeupdate", function () {
+//         progress.value = song.currentTime;
+//         console.log("Changing")
+//     },500);
+
+    progress.addEventListener("input", function () {
+        song.currentTime = progress.value;
+    });
+
+if (song.play()){
+  setInterval(()=>{
+    progress.value = song.currentTime;
+    console.log("Changing")
+  });
 }
 });
